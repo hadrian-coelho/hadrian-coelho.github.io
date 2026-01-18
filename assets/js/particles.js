6111933c9763b4e01753db16139d4e71991a846e
Hadrian <254577971+hadrian-coelho@users.noreply.github.com>
2026-01-18 19:07:00 -0300

    Bolinhas

---
+++ assets/js/particles.js
@@ -0,0 +1,64 @@
+particlesJS("particles-js", {
+    particles: {
+        number: {
+            value: 100,
+            density: {
+                enable: true,
+                value_area: 800
+            }
+        },
+        color: {
+            value: "#9B59B6"
+        },
+        shape: {
+            type: "circle"
+        },
+        opacity: {
+            value: 0.6,
+            anim: {
+                enable: true,
+                speed: 1,
+                opacity_min: 0.1,
+                sync: false
+            }
+        },
+        size: {
+            value: 4,
+            anim: {
+                enable: true,
+                speed: 40,
+                size_min: 0.1,
+                sync: false
+            }
+        },
+        line_linked: {
+            enable: true,
+            distance: 120,
+            color: "#9B59B6",
+            opacity: 0.4,
+            width: 1
+        },
+        move: {
+            enable: true,
+            speed: 2,
+            direction: "none",
+            random: true,
+            straight: false,
+            out_mode: "out"
+        }
+    },
+    interactivity: {
+        detect_on: "canvas",
+        events: {
+            onhover: {
+                enable: true,
+                mode: "repulse"
+            },
+            onclick: {
+                enable: true,
+                mode: "push"
+            }
+        }
+    },
+    retina_detect: true
+});
